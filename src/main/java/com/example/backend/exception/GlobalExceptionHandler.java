31fbf8fd97534dbc4685cca5d81ef703692698d6
Khang080704 <pgkhangt1@gmail.com>
2025-12-26 19:53:29 +0700

    update: add class user dto for response api

--- src/main/java/com/example/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/backend/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.example.backend.exception;
 
 import com.example.backend.dto.ErrorResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,11 +30,25 @@ public class GlobalExceptionHandler {
         return buildResponse(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        log.error(ex.getMessage());
+        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(BadRequestException ex) {
+        log.error(ex.getMessage(), ex);
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUnexpectedError(Exception e) {
         log.error("Unexpected error occur: " + e.getMessage());
         return buildResponse("System error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 
     private ResponseEntity<ErrorResponseDto> buildResponse(String message, HttpStatus httpStatus) {
         ErrorResponseDto responseDto = ErrorResponseDto.builder()
