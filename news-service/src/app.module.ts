966724e87474a77a68eb9c4c1ebf8a7aea1ae9f6
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2026-02-01 14:52:37 +0700

    Implement cache

--- news-service/src/app.module.ts
+++ news-service/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI!), NewsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST!,
+      port: parseInt(process.env.REDIS_PORT!),
+      password: process.env.REDIS_PASSWORD!,
+    }),
+    NewsModule,
+  ],
 })
 export class AppModule {}
